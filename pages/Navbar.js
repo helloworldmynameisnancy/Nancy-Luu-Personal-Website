050238e52356407468714c83519a0ae849aa492d
Nancy Luu <n.luu1330@gmail.com>
2023-09-02 00:05:21 -0500

    changes to navbar

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -7,7 +7,7 @@ function Navbar({ darkMode, setDarkMode }) {
   return (
     <nav className="py-8 mb-48 flex justify-between font-titleFont2 text-emerald-700 dark:text-red-50">
     <Link href="/" legacyBehavior>
-      <a className="text-xl px-16">developedbynancy</a>
+      <a className="text-xl sm:px-8 md:px-12 lg:px-16">developedbynancy</a>
      </Link>
      <ul className="flex items-center">
        <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl fill-emerald-700 dark:fill-white"/>
